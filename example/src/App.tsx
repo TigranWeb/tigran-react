67ce4a0dd6f0e2b35b65bbe0ba5f0296c4bf264a
TigranWeb <tigrank.arm@gmail.com>
2020-08-09 10:30:08 +0400

    first component

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,7 +4,7 @@ import { ExampleComponent } from 'tigran-react'
 import 'tigran-react/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent  />
 }
 
 export default App
